b0ff8d3f1a2b6f1e2a71adf63144cd86bba5d672
shijieSun@chd.edu.cn <shijieSun@chd.edu.cn>
2021-04-08 22:34:10 +0800

    successfully use the actor transformation

--- actor_manager.py
+++ actor_manager.py
@@ -24,7 +24,7 @@ class Actor:
         self.model_path = model_path
         self.createRenderer(layer_num)
         self.loadModel(model_path)
-        self.createBoxWidget()
+        # self.createBoxWidget()
         self.type_class = 0
 
     def readObj(self, model_path):
@@ -85,56 +85,8 @@ class Actor:
         return self.renderer
 
 
-    @staticmethod
-    def boxWidgetInteractionCallback(obj, event):
-        print("boxWidgetInteractionCallback", event)
-        t = vtk.vtkTransform()
-        obj.GetTransform(t)
-        obj.GetProp3D().SetUserTransform(t)
-
-    @staticmethod
-    def boxWidgetInteractionStartCallback(obj, event):
-        print('boxWidgetInteractionStartCallback', event)
-        actor_manager = obj.GetInteractor().GetInteractorStyle().slabel.actor_manager
-        actor = obj.GetProp3D()
-        if actor is actor_manager.getCurrentActiveActor():
-            return 
-        index = actor_manager.getIndex(actor)
-        if index == -1:
-            return
-        actor_manager.setActiveActor(index)
-        print("hello world")
-        pass
-
-    def createBoxWidget(self):
-        # create box widget
-        self.box_widget = vtk.vtkBoxWidget()
-        self.box_widget.SetInteractor(self.interactor)
-        self.box_widget.AddObserver("InteractionEvent", Actor.boxWidgetInteractionCallback)
-        self.box_widget.AddObserver("StartInteractionEvent", Actor.boxWidgetInteractionStartCallback)
-
-        # move the actor to (0, 0, 0)
-        # min_x, _, min_y, _, min_z, _ =self.actor.GetBounds()
-        # transform = vtk.vtkTransform()
-        # transform.Translate(-min_x, -min_y, -min_z)
-
-        self.box_widget.HandlesOff()
-        self.box_widget.ScalingEnabledOff()
-        self.box_widget.SetProp3D(self.actor)
-        self.box_widget.SetPlaceFactor(1.0)
-        self.box_widget.PlaceWidget(self.actor.GetBounds())
-
-        transform = vtk.vtkTransform()
-        pos = self.actor.GetBounds()
-        transform.Translate(-pos[0], -pos[2], -pos[4])
-        self.setUserTransform(transform)
-
-        # boxWidget should be set first and then set the actor
-        # self.box_widget.SetTransform(transform)
-        self.box_widget.On()
-    
     def setUserTransform(self, transform):
-        self.box_widget.SetTransform(transform)
+        # self.box_widget.SetTransform(transform)
         self.actor.SetUserTransform(transform)
 
     @staticmethod
@@ -146,26 +98,10 @@ class Actor:
         dst_camera.SetFocalPoint(src_camera.GetFocalPoint())
         dst_camera.SetViewUp(src_camera.GetViewUp())
 
-        # transform = vtk.vtkTransform()
-        # pos = dst_actor.actor.GetBounds()
-        # transform.Translate(-pos[0], -pos[2], -pos[4])
-        # dst_actor.setUserTransform(transform)
-
         # copy actor parameters
         transform = vtk.vtkTransform()
         transform.DeepCopy(src_actor.actor.GetUserTransform())
         dst_actor.setUserTransform(transform)
-
-        # dst_actor.renderer_window.Render()
-        # dst_camera.SetUserViewTransform(src_camera.GetUserViewTransform())
-        # dst_camera.SetModelTransformMatrix(src_camera.GetModelTransformMatrix())
-
-        # pre_actor = self.actors[-1]
-        # actor.setUserTransform(pre_actor.actor.GetUserTransform())
-        # pre_renderer = self.actors[-1].renderer
-        # camera = pre_renderer.GetActiveCamera()
-        # self.actors[-1].GetActiveCamera().SetUserTransform(camera.GetUserTransform())
-        # self.actors[-1].GetActiveCamera()
 
     def getCameraMatrix(self):
         matrix = self.renderer.GetActiveCamera().GetViewTransformMatrix()
@@ -209,8 +145,8 @@ class ActorManager(QObject):
         self.actors.append(actor)
         self.setActiveActor(-1)
         self.interactor.Render()
-        self.interactor.GetInteractorStyle().OnMouseWheelForward()
-        self.interactor.GetInteractorStyle().OnMouseWheelBackward()
+        # self.interactor.GetInteractorStyle().OnMouseWheelForward()
+        # self.interactor.GetInteractorStyle().OnMouseWheelBackward()
 
         self.reformat()
 
@@ -247,7 +183,7 @@ class ActorManager(QObject):
             actor_transform = getTransform(actor_matrix)
 
             a.actor.SetUserMatrix(vtk.vtkMatrix4x4())
-            a.box_widget.SetTransform(vtk.vtkTransform())
+            # a.box_widget.SetTransform(vtk.vtkTransform())
 
             print(a.actor.GetBounds())
             camera = a.renderer.GetActiveCamera()
--- test1.py
+++ test1.py
@@ -36,15 +36,25 @@ class MyInteractorStyle(vtkInteractorStyleTrackballActor):
         self.AddObserver('MouseMoveEvent', self.OnMouseMove, -1)
         self.AddObserver('MouseWheelForwardEvent', self.OnMouseWheelForward, -1)
         self.AddObserver('MouseWheelBackwardEvent', self.OnMouseWheelBackward, -1)
-        self.isPressed = False
+        self.isPressedRight = False
+        self.isPressedLeft = False
         self.super = super(MyInteractorStyle, self)
+        self.InteractionPicker = vtkCellPicker()
+        self.InteractionProp = None
         
+    def __del__(self):
+        del self.InteractionPicker
+
     def OnLeftButtonDown(self, obj, event):
-        self.isPressed = True
+        self.isPressedLeft = True
         # picker = self.GetInteractor().GetPicker()
         camera = self.GetCurrentRenderer().GetActiveCamera()
         actor = self.GetCurrentRenderer().GetActors().GetLastActor()
         print(actor.GetMatrix())
+
+        x, y = self.GetInteractor().GetEventPosition()
+        self.InteractionPicker.Pick(x, y, 0.0, self.GetCurrentRenderer())
+        self.InteractionProp = self.InteractionPicker.GetViewProp()
 
         # if self.PositionPicked() != 0:
         #    camera.SetFocalPoint(picker.GetPickPosition())
@@ -63,41 +73,141 @@ class MyInteractorStyle(vtkInteractorStyleTrackballActor):
         # print(self.GetActorCenter())
 
     def OnLeftButtonUp(self, obj, event):
-        self.isPressed = False
+        self.isPressedLeft = False
         self.super.OnLeftButtonUp()
         print("OnLeftButtonUp")
 
     def OnRightButtonDown(self, obj, event):
         print("RightButtonDown")
-        self.isPressed = True
+        self.isPressedRight = True
+
+        x, y = self.GetInteractor().GetEventPosition()
+        self.InteractionPicker.Pick(x, y, 0.0, self.GetCurrentRenderer())
+        self.InteractionProp = self.InteractionPicker.GetViewProp()
+
         self.super.OnRightButtonDown()
 
     def OnRightButtonUp(self, obj, event):
         print("RightButtonUp")
-        self.isPressed = False
+        self.isPressedRight = False
         self.super.OnRightButtonUp()
         print(self.GetCurrentRenderer().GetActors().GetLastActor().GetMatrix())
 
 
+    def Prop3DTransform(self, prop3D, boxCenter, numRotation, rotate, scale):
+        oldMatrix = vtkMatrix4x4()
+        prop3D.GetMatrix(oldMatrix)
+        
+        orig = prop3D.GetOrigin()
+
+        newTransform = vtkTransform()
+        newTransform.PostMultiply()
+        if prop3D.GetUserMatrix() is not None:
+            newTransform.SetMatrix(prop3D.GetUserMatrix())
+        else:
+            newTransform.SetMatrix(oldMatrix)
+        
+        newTransform.Translate(-boxCenter[0], -boxCenter[1], -boxCenter[2])
+
+        for i in range(numRotation):
+            newTransform.RotateWXYZ(*(rotate[i][j] for j in range(3)))
+
+        if 0 not in scale:
+            newTransform.Scale(*scale)
+        
+        newTransform.Translate(*boxCenter)
+
+        newTransform.Translate(*(-orig[i] for i in range(3)))
+        newTransform.PreMultiply()
+        newTransform.Translate(*orig)
+
+        if prop3D.GetUserMatrix() is not None:
+            newTransform.GetMatrix(prop3D.GetUserMatrix())
+        else:
+            prop3D.SetPosition(newTransform.GetPosition())
+            prop3D.SetScale(newTransform.GetScale())
+            prop3D.SetOrientation(newTransform.GetOrientation())
+
+        del oldMatrix
+        del newTransform
+
+    def UniformScale(self):
+        if self.GetCurrentRenderer() is None or self.InteractionProp is None:
+            return 
+        rwi = self.GetInteractor()
+        dy = rwi.GetEventPosition()[1] - rwi.GetLastEventPosition()[1]
+        obj_center = self.InteractionProp.GetCenter()
+        center = self.GetCurrentRenderer().GetCenter()
+
+        direction = []
+        camera = self.GetCurrentRenderer().GetActiveCamera()
+        if camera.GetParallelProjection():
+            camera.ComputeViewPlaneNormal()
+            direction = camera.GetViewPlaneNormal()
+        else:
+            cam_x, cam_y, cam_z = self.GetCurrentRenderer().GetActiveCamera().GetPosition()
+            direction = list((obj_center[0] - cam_x, obj_center[1] - cam_y, obj_center[2] - cam_z))
+
+        vtkMath.Normalize(direction)
+
+        # yf = dy / center[1] *  10.0
+        # scaleFactor = 0.1 * (1.1 ** yf)
+        motion_vector = [-0.01 * d * dy for d in direction]
+        if self.InteractionProp.GetUserMatrix() is not None:
+            t = vtkTransform()
+            t.PostMultiply()
+            t.SetMatrix(self.InteractionProp.GetUserMatrix())
+            t.Translate(*motion_vector)
+            self.InteractionProp.GetUserMatrix().DeepCopy(t.GetMatrix())
+            del t
+        else:
+            self.InteractionProp.AddPosition(*motion_vector)
+
+        if self.GetAutoAdjustCameraClippingRange():
+            self.GetCurrentRenderer().ResetCameraClippingRange()
+        
+        rwi.Render()
+
+        # print(obj_center)
+
+        # yf = dy / center[1] *  10.0
+        # scaleFactor = 1.1 ** yf
+        # rotate = None
+        # scale = [scaleFactor]*3
+        # self.Prop3DTransform(self.InteractionProp, obj_center, 0, rotate, scale)
+        # rwi.Render()
+
 
     def OnMouseMove(self, obj, event):
-        if self.isPressed:
-            if self.GetInteractor().GetShiftKey():
-                self.super.OnMouseMove()
-                camera = self.GetCurrentRenderer().GetActiveCamera()
-                print(camera.GetFocalPoint())
-            else:
-                # camera = self.GetCurrentRenderer().GetActiveCamera()
-                # origin = camera.GetFocalPoint()
-                # camera.SetFocalPoint(0, 0, 0)
-                self.super.OnMouseMove()
+        x, y = self.GetInteractor().GetEventPosition()
+
+        if self.isPressedRight and not self.GetInteractor().GetShiftKey():
+            self.FindPokedRenderer(x, y)
+            self.UniformScale()
+            self.InvokeEvent(vtkCommand.InteractionEvent, None)
+            return 
+        # if self.isPressedLeft:
+        #     if self.GetInteractor().GetShiftKey():
+        #         self.super.OnMouseMove()
+        #         camera = self.GetCurrentRenderer().GetActiveCamera()
+        #         print(camera.GetFocalPoint())
+            
+            # else:
+            #     # camera = self.GetCurrentRenderer().GetActiveCamera()
+            #     # origin = camera.GetFocalPoint()
+            #     # camera.SetFocalPoint(0, 0, 0)
+            #     self.FindPokedRenderer(x, y)
+            #     self.UniformScale()
+            #     self.InvokeEvent(vtkCommand.InteractionEvent, None)
+            #     return 
                 # camera.SetFocalPoint(origin)
 
             # camera = self.GetCurrentRenderer().GetActiveCamera()
             # camera.SetFocalPoint(0, 0, 0)
-            camera = self.GetCurrentRenderer().GetActiveCamera()
+            # camera = self.GetCurrentRenderer().GetActiveCamera()
             # actor = self.GetCurrentRenderer().GetActors().GetLastActor()
             # print(camera.GetViewTransformMatrix())
+        self.super.OnMouseMove()
 
     def OnMouseWheelForward(self, obj, event):
         self.super.OnMouseWheelForward()
