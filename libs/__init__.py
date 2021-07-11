99ebde8c9a2970042db7ba391620cf0d01ececbd
Congliang Li <1372331834@qq.com>
2021-07-11 20:32:23 +0800

    add requirement.txt and modify KITTI_2_LabelImg3d

--- libs/__init__.py
+++
@@ -1,2 +0,0 @@
-__version_info__ = ('1', '8', '5')
-__version__ = '.'.join(__version_info__)
--- libs/lKITTI_2_LabelImg3d.py
+++ libs/lKITTI_2_LabelImg3d.py
@@ -6,27 +6,28 @@ import pandas as pd
 
 def KITTI_2_LabelImg3D(label_path, c_distance):
     a = pd.read_table(label_path, sep=' ', header=None)
-    obj_class = a[0][0]
-    obj_position_c = [a[i][0] for i in range(11, 14)]
-    obj_position_w = [obj_position_c[0], -obj_position_c[1], -(obj_position_c[2] - c_distance)]
-    obj_alpha = a[3][0]
-    R_oL2oK = [[0, -1, 0],  # Rotate 90 degrees clockwise around the X axis
-               [1, 0, 0],
-               [0, 0, 1]]
+    model_data = {}
+    data = {}
+    for i in range(0, len(a)):
+        obj_class = a[0][i]
+        obj_position_c = [a[i][i] for i in range(11, 14)]
+        obj_position_w = [obj_position_c[0], -obj_position_c[1], -(obj_position_c[2] - c_distance)]
+        obj_alpha = a[3][i]
+        R_oL2oK = [[0, -1, 0],  # Rotate 90 degrees clockwise around the X axis
+                   [1, 0, 0],
+                   [0, 0, 1]]
 
-    obj_r_y = a[14][0]
-    R_oK2c = roty(obj_r_y)
-    R_c2w_1 = [[0, -1, 0],
-               [1, 0, 0],
-               [0, 0, 1]]
-    R_c2w_2 = [[1, 0, 0],
-               [0, -1, 0],
-               [0, 0, -1]]
-    R_c2w = np.dot(R_c2w_1, R_c2w_2)
-    R_oL2w = np.dot(np.dot(R_oL2oK, R_oK2c), R_c2w)
-    print("\n")
-    print(R_oL2w)
-    # R_oL2w = np.dot(np.dot(R_oL2oK, R_oK2c), R_c2w)
+        obj_r_y = a[14][i]
+        R_oK2c = roty(obj_r_y)
+        R_c2w_1 = [[0, -1, 0],
+                   [1, 0, 0],
+                   [0, 0, 1]]
+        R_c2w_2 = [[1, 0, 0],
+                   [0, -1, 0],
+                   [0, 0, -1]]
+        R_c2w = np.dot(R_c2w_1, R_c2w_2)
+        R_oL2w = np.dot(np.dot(R_oL2oK, R_oK2c), R_c2w)
+
 
     pass
 
