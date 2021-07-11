cd50ebe83a68095d05467d16f818212af9bb14c1
Congliang Li <1372331834@qq.com>
2021-07-11 19:52:18 +0800

    Initially completed the conversion of KITTI to LabelImg3D

--- libs/lKITTI_2_LabelImg3d.py
+++ libs/lKITTI_2_LabelImg3d.py
@@ -4,20 +4,34 @@ import numpy as np
 import pandas as pd
 
 
-def KITTI_2_LabelImg3D(label_path):
+def KITTI_2_LabelImg3D(label_path, c_distance):
     a = pd.read_table(label_path, sep=' ', header=None)
     obj_class = a[0][0]
-    obj_position_c = [a[i][0] for i in range(10, 13)]
+    obj_position_c = [a[i][0] for i in range(11, 14)]
+    obj_position_w = [obj_position_c[0], -obj_position_c[1], -(obj_position_c[2] - c_distance)]
     obj_alpha = a[3][0]
+    R_oL2oK = [[0, -1, 0],  # Rotate 90 degrees clockwise around the X axis
+               [1, 0, 0],
+               [0, 0, 1]]
+
     obj_r_y = a[14][0]
-    R_o2c = roty(obj_r_y)
-    R_c2_w = [[1, 0, 0],
-              [0, 1, 0],
-              [0, 0, 1]]
+    R_oK2c = roty(obj_r_y)
+    R_c2w_1 = [[0, -1, 0],
+               [1, 0, 0],
+               [0, 0, 1]]
+    R_c2w_2 = [[1, 0, 0],
+               [0, -1, 0],
+               [0, 0, -1]]
+    R_c2w = np.dot(R_c2w_1, R_c2w_2)
+    R_oL2w = np.dot(np.dot(R_oL2oK, R_oK2c), R_c2w)
+    print("\n")
+    print(R_oL2w)
+    # R_oL2w = np.dot(np.dot(R_oL2oK, R_oK2c), R_c2w)
 
     pass
 
 
 if __name__ == '__main__':
-    label_path = 'F:/my_desktop/PycharmFiles/3D_detection/labelimg3d/KITTI/label_2/000000.txt'
-    KITTI_2_LabelImg3D(label_path)
+    label_path = 'F:/my_desktop/PycharmFiles/3D_detection/labelimg3d/scenes/000025.txt'
+    distance = 0.52
+    KITTI_2_LabelImg3D(label_path, distance)
