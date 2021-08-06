6ae14fb5d9261ddc11e932106097c6a98a8125d9
Congliang Li <1372331834@qq.com>
2021-08-06 17:30:16 +0800

    add conversion script to EfficentPose

---
+++ libs/utils/labelImg3d_2_EfficentPose.py
@@ -0,0 +1,22 @@
+import json
+
+
+# Convert labelimg3d model.json to EfficentPose models_info.yml
+def model_trans():
+    pass
+
+
+# Convert labelimg3d json to EfficentPose info.yml
+def camera_trans():
+    pass
+
+
+# Convert labelimg3d json to EfficentPose gt.yml
+def img_trans():
+    pass
+
+
+if __name__ == '__main__':
+    model_path = ""
+    camera_path = ""
+    img_path = ""
